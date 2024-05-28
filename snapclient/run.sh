cfea372c0e2de85b07832492d7e77d9767ab32ba
SimpleUserHA <76794676+SimpleUserHA@users.noreply.github.com>
2024-05-28 08:18:05 +0200

    Update run.sh

--- snapclient/run.sh
+++ snapclient/run.sh
@@ -2,16 +2,14 @@
 
 SNAPCLIENT_OPTS=$(bashio::config 'snapclientopts')
 
-PAREC_IP=$(bashio::config 'parecip')
-PAREC_PORT=$(bashio::config 'parecport')
+#PAREC_IP=$(bashio::config 'parecip')
+#PAREC_PORT=$(bashio::config 'parecport')
 
 bashio::log.info "Read Snapclient settings..."
 bashio::log.info "snapclientopts = ${SNAPCLIENT_OPTS}"
 
 bashio::log.info "Version..."
 /usr/bin/snapclient -v
-
-parec -d 1 --rate=44100 --format=s16le | nc ${PAREC_IP} ${PAREC_PORT}
 
 bashio::log.info "Start Snapclient..."
 /usr/bin/snapclient ${SNAPCLIENT_OPTS}
